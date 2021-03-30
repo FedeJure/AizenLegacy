046808d9e239831375765980b8cb893cb6e9c945
Federico Jure <fjure@atixlabs.com>
2021-03-30 11:47:54 -0300

    add store images

---
+++ Assets/Scripts/Character/CharacterStatePositionView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class CharacterStatePositionView : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    
+    void Start()
+    {
+        
+    }
+
+}
