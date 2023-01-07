49aa3411281cae39ccafce0737602ce70582ca1e
Federico Jure <fedejure@gmail.com>
2023-01-07 17:47:33 -0300

    add tutorial animator

---
+++ Assets/GameSection.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class GameSection : MonoBehaviour
+{
+    private string key = "first_time_playing";
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    private void OnEnable()
+    {
+        anim.enabled = false;
+        if (PlayerPrefs.HasKey(key)) return;
+        PlayerPrefs.SetInt(key, 1);
+        anim.enabled = true;
+    }
+}
